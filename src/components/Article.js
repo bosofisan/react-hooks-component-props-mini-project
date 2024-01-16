ee607dc02cc805c8162f4783aa56ad05e69c7cd9
lulu osofisan <boluosofisan@gmail.com>
2024-01-15 23:25:01 -0500

    completed lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,8 +2,7 @@ import React from "react";
 
 
 function Article ({title, date = "January 1, 1970", preview}){
-    // const {title, date, preview} = props;
-    // const formatedDate = date || "January 1,1970";
+
        return (
         <article>
             <h3>{title}</h3>
