7c32fed5191e447179ff726621c50be69bd9d39d
lulu osofisan <boluosofisan@gmail.com>
2024-01-15 23:24:29 -0500

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-import Article from "./Article";
 import ArticleList from "./ArticleList";
+import Article from "./Article";
 
 console.log(blogData);
 
@@ -11,11 +11,9 @@ function App() {
   return (
     <div className="App">
       <Header name = {blogData.name} />
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
       <About image={blogData.image} about={blogData.about} />
-      <Article title={blogData.title} date={blogData.formatedDate} preview={blogData.preview}/>
       <ArticleList posts={blogData.posts} />
+      <Article title={blogData.title} date={blogData.date} preview={blogData.preview}/>
     </div>
   );
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 
-function Article (props){
-    const {title, date, preview} = props;
-    const formatedDate = date || "January 1,1970";
-
+function Article ({title, date = "January 1, 1970", preview}){
+    // const {title, date, preview} = props;
+    // const formatedDate = date || "January 1,1970";
        return (
         <article>
             <h3>{title}</h3>
-            <small>{formatedDate}</small>
+            <small>{date}</small>
             <p>{preview}</p>
         </article>
     );
